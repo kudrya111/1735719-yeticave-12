698b7903ff5ae2b39cde809bff2b9cb0542df175
Egor I Kudriavtsev <Egor.Kudriavtsev@ru.ey.com>
2021-06-06 22:56:26 +0300

    Убрал короткие открывающие теги, заменил '$ad' на '$item'

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ $ads = [
             <li class="promo__item promo__item--boards">
                 <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
             </li>
-            <? endforeach; ?>
+            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -82,18 +82,18 @@ $ads = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach($ads as $ad): ?>
+            <?php foreach($ads as $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $ad["Image"]; ?>" width="350" height="260" alt="">
+                    <img src="<?= $item["Image"]; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $ad["Category"]; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $ad["Name"]; ?></a></h3>
+                    <span class="lot__category"><?= $item["Category"]; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item["Name"]; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $ad["Price"]; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $item["Price"]; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -101,7 +101,7 @@ $ads = [
                     </div>
                 </div>
             </li>
-            <? endforeach; ?>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -115,7 +115,7 @@ $ads = [
             <li class="nav__item">
                 <a href="pages/all-lots.html"><?= $category; ?></a>
             </li>
-            <? endforeach; ?>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
